12c8ff43e98b8661fc3807e70580da7eca7b0e61
chenchuk77 <chenchuk@gmail.com>
2017-02-07 23:07:23 +0200

    adding example: https://jenkins.io/doc/book/pipeline/shared-libraries/

---
+++ vars/acme.groovy
@@ -0,0 +1,14 @@
+// vars/acme.groovy
+class acme implements Serializable {
+    private String name
+    def setName(value) {
+        name = value
+    }
+    def getName() {
+        name
+    }
+    def caution(message) {
+        echo "Hello, ${name}! CAUTION: ${message}"
+    }
+}
+
