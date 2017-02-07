ac5e195dbb65696967531752a947785ea5fd3533
chenchuk77 <chenchuk@gmail.com>
2017-02-07 23:00:31 +0200

    first commit

---
+++ src/net/kukinet/Point.groovy
@@ -0,0 +1,6 @@
+package net.kukinet;
+
+// point in 3d space
+class Point {
+ float x,y,z;
+}
---
+++ src/net/kukinet/Zot.groovy
@@ -0,0 +1,5 @@
+package net.kukinet;
+
+def checkOutFrom(repo){
+  git url: "git@github.com:jenkinsci/${repo}"
+}
