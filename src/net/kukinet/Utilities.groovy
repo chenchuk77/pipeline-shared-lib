f40c3bf94655d06bf991c6c0da8d6cc4d37f50fa
chenchuk <chen.alkabets@nova-lumos.com>
2017-02-08 08:24:16 +0200

    d

---
+++ src/net/kukinet/Utilities.groovy
@@ -0,0 +1,19 @@
+package net.kukinet;
+import static groovy.io.FileType.FILES
+
+def get_artifacts(from_folder) {
+    //from_folder = '/home/chenchuk/dev/unlk/lms'
+    def artifacts = []
+    new File(from_folder).eachFileRecurse(FILES) {
+        if(it.name.endsWith('.war') ||
+                it.name.endsWith('.tar') ||
+                it.name.endsWith('.tar.gz') ) {
+            artifacts.add(it)
+        }
+    }
+    return artifacts
+}
+
+def do_something(){
+  sh "echo 'doing something ...'"
+}
